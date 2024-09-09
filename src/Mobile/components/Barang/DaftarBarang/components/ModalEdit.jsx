692f93ce47106f863a89b9cd9eb6a5dba34b5230
Genta-arya <mgentaarya@gmail.com>
2024-09-09 20:57:30 +0700

    optimasi data

--- src/Mobile/components/Barang/DaftarBarang/components/ModalEdit.jsx
+++ src/Mobile/components/Barang/DaftarBarang/components/ModalEdit.jsx
@@ -163,6 +163,7 @@ const ModalEdit = ({ isOpen, onClose, refresh, data }) => {
               placeholder="Nama Barang"
               value={namaBarang}
               required
+              maxLength={20}
               onChange={handleInputChange(setNamaBarang)}
               className="w-full px-3 py-2 text-xs border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-hijau"
             />
--- src/Mobile/components/Barang/DaftarBarang/components/ModalInput.jsx
+++ src/Mobile/components/Barang/DaftarBarang/components/ModalInput.jsx
@@ -151,6 +151,7 @@ const ModalInput = ({ isOpen, onClose, refresh }) => {
               type="text"
               placeholder="Nama Barang"
               value={namaBarang}
+              maxLength={20}
               required
               onChange={handleInputChange(setNamaBarang)}
               className="w-full px-3 py-2 text-xs border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-hijau"
